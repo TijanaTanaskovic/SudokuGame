accf7b65cad9ba0184a275877d51c4fb9224c01f
TijanaTanaskovic <51413403+TijanaTanaskovic@users.noreply.github.com>
2021-02-14 14:54:10 +0100

    konacna verzija
    mozda su potrebne jos neke dodatne izmene ali mozda nekad

---
+++ Sudoku2/Sudoku/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Sudoku
+{
+    partial class SudokuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.generisiSudokuBtn = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(355, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(667, 615);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // generisiSudokuBtn
+            // 
+            this.generisiSudokuBtn.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generisiSudokuBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.generisiSudokuBtn.Location = new System.Drawing.Point(28, 90);
+            this.generisiSudokuBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.generisiSudokuBtn.Name = "generisiSudokuBtn";
+            this.generisiSudokuBtn.Size = new System.Drawing.Size(263, 58);
+            this.generisiSudokuBtn.TabIndex = 1;
+            this.generisiSudokuBtn.Text = "Generiši sudoku";
+            this.generisiSudokuBtn.UseVisualStyleBackColor = true;
+            this.generisiSudokuBtn.Click += new System.EventHandler(this.generisiSudokuBtn_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(55, 222);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(208, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 187);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Težina";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(28, 545);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 59);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Pravila";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SudokuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 649);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.generisiSudokuBtn);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "SudokuForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button generisiSudokuBtn;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Sudoku2/Sudoku/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class SudokuForm : Form
+    {
+        public SudokuPolje[,] polja = new SudokuPolje[9, 9];
+        int[,] startMat;
+        int tezina = 21;
+
+        public SudokuForm()
+        {
+            InitializeComponent();
+
+            trackBar1.Value = 3;
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var polje = new SudokuPolje(i, j, this);
+                    polje.Location = new System.Drawing.Point(i * 500 / 9, j * 500 / 9);
+                    polje.Size = new Size(40, 40);
+                    polje.MaxLength = 1;
+                    polje.TextAlign = HorizontalAlignment.Center;
+                    polje.Font = new Font("Arial", 24, FontStyle.Bold);
+                    polje.Enabled = false;
+                    panel1.Controls.Add(polje);
+                    polja[i, j] = polje;
+                   
+
+                }
+            }
+
+            pixelArt();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Brush color = Brushes.DarkGray;
+
+            Rectangle r1 = new Rectangle(500 / 3 - 10, 0, 5, 500);
+            Rectangle r2 = new Rectangle(500 * 2 / 3 - 10, 0, 5, 500);
+            Rectangle r3 = new Rectangle(0, 500 / 3 - 8, 500, 5);
+            Rectangle r4 = new Rectangle(0, 500 * 2 / 3 - 8, 500, 5);
+
+            e.Graphics.FillRectangle(color, r1);
+            e.Graphics.FillRectangle(color, r2);
+            e.Graphics.FillRectangle(color, r3);
+            e.Graphics.FillRectangle(color, r4);
+        }
+        private void generisiSudokuBtn_Click(object sender, EventArgs e)
+        {
+            SudokuGenerator gen = new SudokuGenerator(9, tezina);
+            gen.fillValues();
+            startMat = gen.getSudoku();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    polja[i, j].Text = "";
+                    polja[i, j].ForeColor = Color.Black;
+                    polja[i, j].BackColor = Color.White;
+                    polja[i, j].Enabled = true;
+                    if (startMat[i, j] != 0)
+                    {
+                        polja[i, j].ForeColor = Color.Blue;
+                        polja[i, j].Text = startMat[i, j].ToString();
+                        polja[i, j].Enabled = false;
+                    }
+                }
+            }
+        }
+        private void pixelArt()
+        {
+            int[,] map = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           {0, 0, 1, 1, 0, 1, 1, 0, 0 },
+                           {0, 1, 1, 1, 1, 1, 1, 1, 0 },
+                           {0, 1, 1, 1, 1, 1, 1, 1, 0 },
+                           {0, 0, 1, 1, 1, 1, 1, 0, 0},
+                           {0, 0, 0, 1, 1, 1, 0, 0, 0},
+                           {0, 0, 0, 0, 1, 0, 0, 0, 0},
+                           {0, 0, 0, 0, 0, 0, 0, 0, 0}
+                           };
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    if (map[i, j] == 1)
+                        polja[j, i].BackColor = Color.Red;
+                    else polja[j, i].BackColor = Color.White;
+                }
+            }
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            tezina = trackBar1.Value*6+4;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pravila su sledeća: Cilj je popuniti sva prazna polja, tako da se u istoj koloni, istom redi i u istom kvadratu(3x3) ne ponovi nijedan broj.");
+        }
+    }
+}
